5ebbaf53a99a8f0ccd46ed6da3350801f5112997
karolkujawski <kjawsk@gmail.com>
2017-04-30 16:44:56 +0200

    added test template

--- stats_parser/stats_parser/items.py
+++ stats_parser/stats_parser/items.py
@@ -8,7 +8,8 @@
 import scrapy
 
 
-class StatsParserItem(scrapy.Item):
+class TeamItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    players = scrapy.Field()
--- stats_parser/stats_parser/spiders/starting_five.py
+++ stats_parser/stats_parser/spiders/starting_five.py
@@ -5,11 +5,21 @@ class StartingFiveSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://plk.pl/mecz/45043/polski-cukier-torun---anwil-wloclawek.html',
+            "http://plk.pl/mecz/45043/polski-cukier-torun---anwil-wloclawek.html",
+            "http://plk.pl/mecz/45038/mks-dabrowa-gornicza---energa-czarni-slupsk.html",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for row in response.xpath("//tr[td//i[@class='ico-star']]"):
-            yield {'nazwisko': row.xpath(".//a//strong/text()").extract()}
+        teamB_players = []
+        teamA_players = []
+        for idx, row in enumerate(response.xpath("//tr[td//i[@class='ico-star']]")):
+            if idx < 5:
+                teamA_players.append(row.xpath(".//a//strong/text()").extract_first())
+            elif 4 < idx < 10:
+                teamB_players.append(row.xpath(".//a//strong/text()").extract_first())
+            else:
+                raise ValueError("There is more than 10 players in starting fives")
+        return {"teamA" : teamA_players,
+                "teamB" : teamB_players}
