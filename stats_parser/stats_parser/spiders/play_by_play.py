55504daa1c566b28f7dae8e9e37131c09ea9353b
karolkujawski <kjawsk@gmail.com>
2017-05-02 14:39:12 +0200

    Implemented extraction of all actions

--- stats_parser/stats_parser/spiders/play_by_play.py
+++ stats_parser/stats_parser/spiders/play_by_play.py
@@ -19,11 +19,25 @@ class PlayByPlaySpider(scrapy.Spider):
         result = [' '.join(x) for x in result]
         return result
 
-    def parse(self, response):
-        extracted = response.xpath(x_play_by_play).extract()
+    def get_actions_from_quart(self,response, quart):
+        xpath = x_play_by_play[quart]
+        extracted = response.xpath(xpath).extract()
         cleaned = [remove_tags(x) for x in extracted if x != '<td>\xa0</td>']
         linked = [cleaned[i]+cleaned[i-1] for i in range(len(cleaned)) if i % 2 == 0]
-        actions = self.remove_whitespaces(linked)[2:] #first two plays are always empty => "10:00"
+        #for 1st quart first two plays are always empty => "10:00"
+        #for 2nd-4th quart first play are always empty => "10:00"
+        offset = 2 if quart == 1 else 1
+        #for 4th quart last action is empty
+        if quart == 4:
+            return self.remove_whitespaces(linked)[offset:-1]
+        else:
+            return self.remove_whitespaces(linked)[offset:]
+
+    def parse(self, response):
+        actions = self.get_actions_from_quart(response, 1)
+        actions += self.get_actions_from_quart(response, 2)
+        actions += self.get_actions_from_quart(response, 3)
+        actions += self.get_actions_from_quart(response, 4)
         regex = re.compile("\d{2}:\d{2}")
         for action in actions:
             time = regex.search(action).group()
--- stats_parser/stats_parser/spiders/xpaths.py
+++ stats_parser/stats_parser/spiders/xpaths.py
@@ -2,7 +2,28 @@ x_teams_name = "//div[@class='game-team']//h3//a/text()"
 x_game_tables = "//table[@class='game-table']"
 x_starting_players = ".//tr[td//i[@class='ico-star']]//a//strong/text()"
 x_bench_players = ".//tr[@class='row_stat']//td[@class='zawodnik']//a/text()"
-x_play_by_play = \
+x_play_by_play_q1 = \
     "//div[@class='tab tab0 current']//table[@id='playbyplay']//tbody//tr//td[1] | \
     //div[@class='tab tab0 current']//table[@id='playbyplay']//tbody//tr//td[3]  | \
     //div[@class='tab tab0 current']//table[@id='playbyplay']//tbody//tr//td[5]"
+
+x_play_by_play_q2 = \
+    "//div[@id='playbyplay-wrapper']//div[@class='tab tab1']//table[@id='playbyplay']//tbody//tr//td[1] | \
+     //div[@id='playbyplay-wrapper']//div[@class='tab tab1']//table[@id='playbyplay']//tbody//tr//td[3]  | \
+     //div[@id='playbyplay-wrapper']//div[@class='tab tab1']//table[@id='playbyplay']//tbody//tr//td[5]"
+
+x_play_by_play_q3 = \
+    "//div[@class='tab tab2']//table[@id='playbyplay']//tbody//tr//td[1] | \
+    //div[@class='tab tab2']//table[@id='playbyplay']//tbody//tr//td[3]  | \
+    //div[@class='tab tab2']//table[@id='playbyplay']//tbody//tr//td[5]"
+
+x_play_by_play_q4 = \
+    "//div[@class='tab tab3']//table[@id='playbyplay']//tbody//tr//td[1] | \
+    //div[@class='tab tab3']//table[@id='playbyplay']//tbody//tr//td[3]  | \
+    //div[@class='tab tab3']//table[@id='playbyplay']//tbody//tr//td[5]"
+
+x_play_by_play = {
+    1: x_play_by_play_q1,
+    2: x_play_by_play_q2,
+    3: x_play_by_play_q3,
+    4: x_play_by_play_q4}
