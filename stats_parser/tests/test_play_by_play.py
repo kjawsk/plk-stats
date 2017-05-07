3f2e3516c329dbbaa75b388e699ef4a91eac21c8
karolkujawski <kjawsk@gmail.com>
2017-05-07 21:20:35 +0200

    Changed to check response against prepared output

--- stats_parser/tests/test_play_by_play.py
+++ stats_parser/tests/test_play_by_play.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import ast
 from stats_parser.spiders.play_by_play import PlayByPlaySpider
 from responses import fake_response_from_file
 
@@ -14,16 +15,11 @@ class StartingLineupTest(unittest.TestCase):
         results = self.spider.parse(
             fake_response_from_file('samples/torun_wloclawek.html')
         )
-        expected = {
-            "team":"away",
-            "time":"09:42",
-            "action":"F. Dmitriew - faul osobisty (1 F, 1 Faul zespołu). Faulowany: K. Sulima",
-        }
-        self.assertEqual(next(results), expected)
 
-        expected = {
-            "team":"home",
-            "time":"09:32",
-            "action":"K. Sulima - celny lay-up za 2 pkt (2 Pkt). K. Weaver - asysta (1 AS)",
-        }
-        self.assertEqual(next(results), expected)
+        path = "tests/responses/samples/play_by_play_output.json"
+        with open(path) as plays:
+            for play in plays:
+                result = next(results)
+                play = play.replace("},", "}").strip()
+                play = ast.literal_eval(play)
+                self.assertEqual(play, result, msg="\nPlay: %s\nResult: %s" % (play, result))
