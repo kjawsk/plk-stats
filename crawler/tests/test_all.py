379eadc75222cf4fe2b53d241c9c759585833461
karolkujawski <kjawsk@gmail.com>
2017-09-18 22:10:24 +0200

    Fixed test to be able to test with pytest

--- crawler/tests/test_all.py
+++ crawler/tests/test_all.py
@@ -1,10 +1,6 @@
-"""
-Test are created mainly for refactoring purposes, they use fake_response_from_file function from
-responses module.
-"""
+"""Tests for django-scrapy integration"""
 
-import ast
-
+import datetime
 import sys
 sys.path.append("/home/karol/Projects/plkStats/page/")
 import os
@@ -12,13 +8,32 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "page.settings"
 import django
 django.setup()
 
-from crawler.items import TeamItem
-from stats.models import Team
+from django.test import TestCase
+from crawler.items import TeamItem, MatchItem, ActionItem
+from stats.models import Team, Match, Action, Player, Action_Type
 
-def test_item_is_saved_in_db():
-    """Test for scrapy - django orm integration"""
-    before = Team.objects.count()
-    team_item = TeamItem(name='new team')
-    team_item.save()
+class DjangoScrapyIntegrationTests(TestCase):
+    """Tests for django-scrapy integration"""
 
-    assert Team.objects.count() == before+1
+    def test_scrapy_items_are_saved_in_db(self):
+        """Checks if scrapy items(Team, Match and Action) are saved in databse
+        """
+        home_team = TeamItem(name='new team').save()
+        assert Team.objects.count() == 1
+
+        away_team = TeamItem(name='new team').save()
+        assert Team.objects.count() == 2
+
+        date = datetime.datetime.now()
+        match = MatchItem(home_team=home_team, away_team=away_team, date=date).save()
+        assert Match.objects.count() == 1
+
+        action_type = Action_Type.objects.create(name="2PKTC")
+        player = Player.objects.create(name="John Doe", team=home_team)
+        ActionItem(
+            match=match,
+            action_type=action_type,
+            player=player,
+            time=datetime.datetime.now()
+        ).save()
+        assert Action.objects.count() == 1
