9087c6a81bbe3b614b80e8341836961a8b88870b
kjawsk <kjawsk@gmail.com>
2017-12-04 21:34:50 +0100

    adapted to adding matches by ids spider

--- crawler/crawler/spiders/actions.py
+++ crawler/crawler/spiders/actions.py
@@ -1,12 +1,13 @@
 """This is a module spider for scraping actions from matches
 """
 
-import scrapy
-import json
 import datetime;
+import json
+import scrapy
+import re
 
 from crawler.items import ActionItem, MatchItem
-from stats.models import Action, Action_Type, Action_Subtype, Player, Team
+from stats.models import Action, Action_Type, Action_Subtype, Player, Team, Match
 
 class ActionsSpider(scrapy.Spider):
 
@@ -20,30 +21,22 @@ class ActionsSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def add_match(self):
-        """"""
-        try:
-            home_team_name = self.data['tm']['1']['name']
-            away_team_name = self.data['tm']['2']['name']
-            self.home_team = Team.objects.get(name=home_team_name)
-            self.away_team = Team.objects.get(name=away_team_name)
-        except Team.DoesNotExist:
-            self.logger.critical(
-                "%s, %s: one of these team names not found in db during parsing: %s" %
-                (home_team_name, away_team_name, self.response.url)
-            )
-            raise
-        except KeyError:
-            self.logger.critical(
-                "Name field not found in json from %s" % (self.response.url)
-            )
-        else:
-            match = MatchItem()
-            match['home_team'] = self.home_team
-            match['away_team'] = self.away_team
-            match['date'] = datetime.datetime.now()
-            match = match.save()
-            return match
+    def match_for_response(self, response):
+        fiba_id = re.search(r'\d+', response.url).group()
+        if fiba_id is not None:
+            try:
+                match = Match.objects.get(fiba_id=fiba_id)
+            except Match.DoesNotExist:
+                self.logger.critical(
+                    "\n------\Match with following fiba_id does not exist in db: %s\n"%
+                    (fiba_id)
+                )
+            else:
+                return match
+        self.logger.critical(
+            "\n------\Fiba id not found in following url: %s\n"%
+            (response.url)
+        )
 
     def action_type(self, play):
         try:
@@ -61,14 +54,15 @@ class ActionsSpider(scrapy.Spider):
                 (play['actionType'])
             )
             raise
-        return {
-            'type' : action_type,
-            'subtype' : action_subtype,
-        }
+        finally:
+            return {
+                'type' : action_type,
+                'subtype' : action_subtype,
+            }
 
     def parse(self, response):
         self.data = json.loads(response.body_as_unicode())
-        match = self.add_match()
+        match = self.match_for_response(response)
 
         for play in self.data['pbp']:
             if play['actionType'] == '2pt':
@@ -76,7 +70,7 @@ class ActionsSpider(scrapy.Spider):
                 play_type = self.action_type(play)
                 item = ActionItem()
                 item['match'] = match
-                item['team'] = self.home_team if play['tno'] == 1 else self.away_team
+                item['team'] = match.home_team if play['tno'] == 1 else match.away_team
                 item['action_type'] = play_type['type']
                 item['action_subtype'] = play_type['subtype']
                 item['player'] = Player.objects.get(name=player_name)
