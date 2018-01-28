ac056ea0e5ab56eb1b98247749a99217af8df018
kjawsk <kjawsk@gmail.com>
2018-01-28 20:55:18 +0100

    Action spider has been split to separete databse operations

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,5 +1,7 @@
+import logging
+
 from datetime import datetime
-from stats.models import Team, Player, Team_Player
+from stats.models import Action, Action_Type, Action_Subtype, Team, Team_Player, Match, Period_Type, Player
 
 class PlayersPipeline(object):
     """This is a class responsible for processing(adding to database) items from players spider"""
@@ -45,4 +47,78 @@ class PlayersPipeline(object):
         team, _ = Team.objects.get_or_create(name=item['team_name'])
         self._current_players(item['current_players'], team)
         self._past_players(item['past_players'], team)
+        return item
+
+class ActionsPipeline(object):
+    """This is a class responsible for processing(adding to database) items from actions spider"""
+
+    def _match(self, fiba_id):
+        """"Handles fetching match from db based on fiba_id, if specified match is not found
+        exception is raised"""
+        try:
+            match = Match.objects.get(fiba_id=fiba_id)
+        except Match.DoesNotExist:
+            self.logger.critical(
+                "\n------\Match with following fiba_id does not exist in db: %s\n"%
+                (fiba_id)
+            )
+            raise
+        else:
+            return match
+
+    def _player(self, player_name, team):
+        """Handles fetching player for specified play, if player is not found
+        exception is raised"""
+        try:
+            player = Team_Player.objects.get(player__name__iexact=player_name, team=team)
+        except Team_Player.DoesNotExist:
+            self.logger.critical(
+                "\n------\nTeamPlayer does not exist: %s %s\n"%
+                (team.name, player_name)
+            )
+            raise
+        except Team_Player.MultipleObjectsReturned:
+            self.logger.critical(
+                "\n------\nThere are more than one player: %s %s\n"%
+                (team.name, player_name)
+            )
+            raise
+        else:
+            return player
+
+    def _team(self, team_name):
+        """Handles fetching team for specified play, if team is not found exception is raised"""
+        try:
+            team = Team.objects.get(name=team_name)
+        except Team.DoesNotExist:
+            self.logger.critical(
+                "\n------\nTeam does not exist: %s\n" % (team_name)
+            )
+            raise
+        else:
+            return team
+
+    def process_item(self, item, spider):
+        """Processes items from actions spider"""
+        self.logger = logging.getLogger('Actions Pipeline Logger')
+        self.logger.setLevel(logging.CRITICAL)
+
+        match = self._match(item['fiba_id'])
+        team = self._team(item['team'])
+        action_type, _ = Action_Type.objects.get_or_create(name=item['action_type'])
+        action_subtype, _  = Action_Subtype.objects.get_or_create(name=item['action_subtype'])
+        player = self._player(item['player_name'],team) if item['player_name'] is not None else None
+        period_type, _ = Period_Type.objects.get_or_create(name=item['period_type'])
+
+        Action.objects.create(
+            match=match,
+            action_type=action_type,
+            action_subtype=action_subtype,
+            teamplayer=player,
+            time=item['time'],
+            success=item['success'],
+            period_type=period_type,
+            period=item['period'],
+        )
+
         return item
--- crawler/crawler/spiders/actions.py
+++ crawler/crawler/spiders/actions.py
@@ -7,86 +7,55 @@ import scrapy
 import re
 
 from django.db.models import Count
-from stats.models import Action, Action_Type, Action_Subtype, Team_Player, Match, Period_Type
+from stats.models import Match
 
 class ActionsSpider(scrapy.Spider):
-    """Class ActionsSpider is responsible for fetching actions from fibalivestats for matches
-    defined in urls variable(in start_requests)"""
+    """Class ActionsSpider is responsible for fetching actions from fibalivestats"""
 
     name = "actions"
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'crawler.pipelines.ActionsPipeline': 300
+        }
+    }
 
     def start_requests(self):
-        """Provides list of matches to scrap"""
+        """Provides list of matches to scrap - matches without actions"""
         matches = Match.objects.annotate(Count('action')).filter(action__count=0)
         for match in matches:
             url = "http://www.fibalivestats.com/data/%s/data.json" % (match.fiba_id)
             yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url=url, callback=self.parse)
 
-    def _match_from_response(self, response):
-        """"Handles fetching match from db based on fiba_id, if specified match is not found
-        exception is raised"""
-        fiba_id = re.search(r'\d+', response.url).group()
-        if fiba_id is not None:
-            try:
-                match = Match.objects.get(fiba_id=fiba_id)
-            except Match.DoesNotExist:
-                self.logger.critical(
-                    "\n------\Match with following fiba_id does not exist in db: %s\n"%
-                    (fiba_id)
-                )
-                raise
-            else:
-                return match
-        self.logger.critical(
-            "\n------\Fiba id not found in following url: %s\n"%
-            (response.url)
-        )
-
-    def _player(self, play, team):
-        """Handles fetching player for specified play, if player is not found
-        exception is raised"""
+    def _player(self, play):
+        """Handles fetching player for play, if KeyError is raised, then None is returned"""
         try:
             player_name = play['firstName'] + " " + play['familyName']
-            player = Team_Player.objects.get(player__name__iexact=player_name, team=team)
-        except Team_Player.DoesNotExist:
-            self.logger.critical(
-                "\n------\nTeamPlayer does not exist: %s %s\n"%
-                (team.name, player_name)
-            )
-            raise
-        except Team_Player.MultipleObjectsReturned:
-            self.logger.critical(
-                "\n------\nThere are more than one player: %s %s\n"%
-                (team.name, player_name)
-            )
-            raise
         except KeyError:
-            player = None
+            player_name = None
         else:
-            return player
+            return player_name
 
     def parse(self, response):
-        """Builds action model object for each fetched action from fibalivestats"""
-        self.data = json.loads(response.body_as_unicode())
-        match = self._match_from_response(response)
+        """Builds action dicts for all plays from fibalivestats match data
 
-        for play in self.data['pbp']:
-            team = match.home_team if play['tno'] == 1 else match.away_team
-            action_type, created = Action_Type.objects.get_or_create(name=play['actionType'])
-            action_subtype, created  = Action_Subtype.objects.get_or_create(name=play['subType'])
-            player = self._player(play, team)
-            time = datetime.datetime.strptime(play['gt'], '%M:%S').time()
-            success = True if play['success'] == 1 else False
-            period_type, created = Period_Type.objects.get_or_create(name=play['periodType'])
-            period =  play['period']
+        @url http://www.fibalivestats.com/data/780639/data.json
+        @returns items 486 486
+        """
+        fiba_id = re.search(r'\d+', response.url).group()
+        data = json.loads(response.body_as_unicode())
+        home_team_name = data['tm']['1']['name']
+        away_team_name = data['tm']['2']['name']
 
-            Action.objects.create(
-                match= match,
-                action_type=action_type,
-                action_subtype=action_subtype,
-                teamplayer=player,
-                time=time,
-                success=success,
-                period_type=period_type,
-                period=period,
-            )
+        for play in data['pbp']:
+            action = dict()
+            action['fiba_id'] = fiba_id
+            action['team'] = home_team_name if play['tno'] == 1 else away_team_name
+            action['action_type'] = play['actionType']
+            action['action_subtype'] = play['subType']
+            action['player_name'] = self._player(play)
+            action['time'] = datetime.datetime.strptime(play['gt'], '%M:%S').time()
+            action['success'] = True if play['success'] == 1 else False
+            action['period_type'] = play['periodType']
+            action['period'] =  play['period']
+            yield action
