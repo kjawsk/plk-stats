a6b3898ddd014e4b726ebc5b8cf56fcd48547513
kjawsk <kjawsk@gmail.com>
2018-02-01 20:25:26 +0100

    Linter fixes

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,7 +1,8 @@
 import logging
 
 from datetime import datetime
-from stats.models import Action, Action_Type, Action_Subtype, Team, Team_Player, Match, Period_Type, Player
+from stats.models import (Action, Action_Type, Action_Subtype, Team, Team_Player, Match,
+                          Period_Type, Player)
 
 
 class PlayersPipeline(object):
@@ -14,13 +15,13 @@ class PlayersPipeline(object):
         players_in_db = Team_Player.objects.filter(team=team)
         for player in current_players:
             if not players_in_db.filter(player__name=player[2]).exists():
-                player, created = Player.objects.get_or_create(
-                    name = player[2],
-                    short_name = player[2].split()[0][0] + ". " + player[2].split()[1],
-                    passport = player[3],
-                    birth = datetime.strptime(player[4], "%Y-%m-%d"),
-                    height = player[5],
-                    position = player[6]
+                player, _ = Player.objects.get_or_create(
+                    name=player[2],
+                    short_name=player[2].split()[0][0] + ". " + player[2].split()[1],
+                    passport=player[3],
+                    birth=datetime.strptime(player[4], "%Y-%m-%d"),
+                    height=player[5],
+                    position=player[6]
                 )
                 Team_Player.objects.create(
                     team=team,
@@ -35,10 +36,10 @@ class PlayersPipeline(object):
         players_in_db = Team_Player.objects.filter(team=team)
         for player in past_players:
             if not players_in_db.filter(player__name=player).exists():
-                player, created  = Player.objects.get_or_create(
-                        name=player,
-                        short_name=player.split()[0][0] + ". " + player.split()[1]
-                    )
+                player, _ = Player.objects.get_or_create(
+                    name=player,
+                    short_name=player.split()[0][0] + ". " + player.split()[1]
+                )
                 Team_Player.objects.create(
                     team=team,
                     player=player,
@@ -56,6 +57,11 @@ class PlayersPipeline(object):
 class ActionsPipeline(object):
     """This is a class responsible for processing(adding to database) items from actions spider"""
 
+    def __init__(self):
+        self.logger = logging.getLogger('Actions Pipeline Logger')
+        self.logger.setLevel(logging.CRITICAL)
+        super().__init__()
+
     def _match(self, fiba_id):
         """"Handles fetching match from db based on fiba_id, if specified match is not found
         exception is raised"""
@@ -63,8 +69,7 @@ class ActionsPipeline(object):
             match = Match.objects.get(fiba_id=fiba_id)
         except Match.DoesNotExist:
             self.logger.critical(
-                "\n------\Match with following fiba_id does not exist in db: %s\n"%
-                (fiba_id)
+                "\n------\nMatch with following fiba_id does not exist in db: %s\n" % (fiba_id)
             )
             raise
         else:
@@ -104,13 +109,11 @@ class ActionsPipeline(object):
 
     def process_item(self, item, spider):
         """Processes items from actions spider"""
-        self.logger = logging.getLogger('Actions Pipeline Logger')
-        self.logger.setLevel(logging.CRITICAL)
 
         match = self._match(item['fiba_id'])
         team = self._team(item['team'])
         action_type, _ = Action_Type.objects.get_or_create(name=item['action_type'])
-        action_subtype, _  = Action_Subtype.objects.get_or_create(name=item['action_subtype'])
+        action_subtype, _ = Action_Subtype.objects.get_or_create(name=item['action_subtype'])
         player = self._player(item['player_name'],team) if item['player_name'] is not None else None
         period_type, _ = Period_Type.objects.get_or_create(name=item['period_type'])
 
