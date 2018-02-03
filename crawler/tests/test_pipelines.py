89feda1260d14fba5bf44bdf9a79ed0646ba5f33
kjawsk <kjawsk@gmail.com>
2018-02-03 12:29:02 +0100

    Generators implemented for expected values

--- crawler/tests/test_pipelines.py
+++ crawler/tests/test_pipelines.py
@@ -13,24 +13,39 @@ from crawler.items import PlayerItem, TeamPlayersItem
 from stats.models import Team, Player, Team_Player
 from crawler.pipelines import PlayersPipeline
 
+PLAYERS = [
+    ("John Doe", "J. Doe", "USA", "1990-05-27", "198", "shooting guard"),
+    ("Kobe Bry", "K. Bry", "USA", "1991-06-27", "188", "point guard"),
+    ("Big Mike", "B. Mike", "USA", "1992-07-27", "178", "small forward"),
+    ("Ty Law", "T. Law", "USA", "1993-08-27", "218", "center"),
+]
 
-def player_item(name):
-    return PlayerItem(
-        name=name,
-        short_name=name.split()[0][0] + ". " + name.split()[1],
-        passport="USA",
-        birth="1990-08-27",
-        height="198",
-        position="point guard"
-    )
+
+def players():
+    for idx, _ in enumerate(PLAYERS):
+        yield PLAYERS[idx]
+
+
+def player_items():
+    for player in players():
+        name, short_name, passport, birth, height, position = player
+        yield PlayerItem(
+            name=name,
+            short_name=short_name,
+            passport=passport,
+            birth=birth,
+            height=height,
+            position=position
+        )
 
 
 @pytest.fixture
 def team_players_item():
-    player_1 = player_item("John Doe")
-    player_2 = player_item("Kobe Bry")
-    player_3 = player_item("Big Mike")
-    player_4 = player_item("Ty Law")
+    items = player_items()
+    player_1 = items.__next__()
+    player_2 = items.__next__()
+    player_3 = items.__next__()
+    player_4 = items.__next__()
     return TeamPlayersItem(
         team_name="New Team",
         current_players=[player_1, player_2],
@@ -40,10 +55,11 @@ def team_players_item():
 
 @pytest.fixture
 def team_players_item2():
-    player_1 = player_item("John Doe")
-    player_2 = player_item("Kobe Bry")
-    player_3 = player_item("Big Mike")
-    player_4 = player_item("Ty Law")
+    items = player_items()
+    player_1 = items.__next__()
+    player_2 = items.__next__()
+    player_3 = items.__next__()
+    player_4 = items.__next__()
     return TeamPlayersItem(
         team_name="Old Team",
         current_players=[player_1, player_2],
@@ -67,44 +83,38 @@ def test_team_is_not_created_when_it_already_exists(team_players_item):
 def test_players_are_created(team_players_item):
     PlayersPipeline().process_item(team_players_item, "")
 
-    assert Player.objects.filter(name="John Doe").exists()
-    assert Player.objects.filter(name="Kobe Bry").exists()
-    assert Player.objects.filter(name="Big Mike").exists()
-    assert Player.objects.filter(name="Ty Law").exists()
+    for player in players():
+        assert Player.objects.filter(name=player[0]).exists()
 
 
 def test_team_players_are_created(team_players_item):
     PlayersPipeline().process_item(team_players_item, "")
 
-    assert Team_Player.objects.filter(player__name="John Doe", team__name="New Team").exists()
-    assert Team_Player.objects.filter(player__name="Kobe Bry", team__name="New Team").exists()
-    assert Team_Player.objects.filter(player__name="Big Mike", team__name="New Team").exists()
-    assert Team_Player.objects.filter(player__name="Ty Law", team__name="New Team").exists()
+    for player in players():
+        assert Team_Player.objects.filter(player__name=player[0], team__name="New Team").exists()
 
 
 def test_player_is_not_created_when_it_already_exists(team_players_item):
     PlayersPipeline().process_item(team_players_item, "")
     PlayersPipeline().process_item(team_players_item, "")
 
-    assert Player.objects.filter(name="John Doe").count() == 1
-    assert Player.objects.filter(name="Ty Law").count() == 1
+    for player in players():
+        assert Player.objects.filter(name=player[0]).count() == 1
 
 
 def test_team_player_is_not_created_when_it_already_exists(team_players_item):
     PlayersPipeline().process_item(team_players_item, "")
     PlayersPipeline().process_item(team_players_item, "")
 
-    assert Team_Player.objects.filter(player__name="Kobe Bry", team__name="New Team").count() == 1
-    assert Team_Player.objects.filter(player__name="Big Mike", team__name="New Team").count() == 1
+    for player in players():
+        assert Team_Player.objects.filter(player__name=player[0], team__name="New Team").count() == 1
 
 
 def test_once_created_player_can_be_added_to_two_teams(team_players_item, team_players_item2):
     PlayersPipeline().process_item(team_players_item, "")
     PlayersPipeline().process_item(team_players_item2, "")
 
-    assert Player.objects.filter(name="John Doe").count() == 1
-    assert Player.objects.filter(name="Ty Law").count() == 1
-    assert Team_Player.objects.filter(player__name="John Doe", team__name="New Team").count() == 1
-    assert Team_Player.objects.filter(player__name="Ty Law", team__name="New Team").count() == 1
-    assert Team_Player.objects.filter(player__name="John Doe", team__name="Old Team").count() == 1
-    assert Team_Player.objects.filter(player__name="Ty Law", team__name="Old Team").count() == 1
+    for player in players():
+        assert Player.objects.filter(name=player[0]).count() == 1
+        assert Team_Player.objects.filter(player__name=player[0], team__name="New Team").count() == 1
+        assert Team_Player.objects.filter(player__name=player[0], team__name="Old Team").count() == 1
